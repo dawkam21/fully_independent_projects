227a0e3c95723e4a20e2905f94fad592f311fbf9
dawkam21 <dawid.jan.kaminski@gmail.com>
2026-03-08 10:32:11 +0100

    edit notes finished, now problem with deleting

--- notepad_cpp/main.cpp
+++ notepad_cpp/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+#include <sstream>
 
 int main() {
     
     std::vector<std::string> notes;
-    
+    bool isFalse = false;
     int choice;
-    while (true) {
+    while (!isFalse) {
+        std::cout << std::endl;
         std::cout << "-------------------" << std::endl;
         std::cout << "1. Add note " << std::endl;
         std::cout << "2. Show notes " << std::endl;
@@ -17,32 +18,66 @@ int main() {
         std::cout << "5. Exit " << std::endl;
         std::cout << "-------------------" << std::endl;
 
-        std::cout << "Pick an option:" << std::endl;
+        std::cout << std::endl << "Pick an option:" << std::endl;
         std::cin >> choice;
         std::cin.ignore();
 
+        int counter = 0;
+        std::string note;
         if (choice == 1) {
-            std::string note;
-            std::cout << "Make a note: " << std::endl;
+            std::cout << std::endl << "Make a note: " << std::endl;
             std::getline(std::cin, note);
             notes.push_back(note);
-            std::cout << "Your note: " << note << std::endl;
+            std::cout << std::endl << "Your note: " << note << std::endl;
         } else if (choice == 2) {
-            std::cout << "All your notes: " << std::endl;
-            int counter = 0;
+            std::cout << std::endl << "All your notes: " << std::endl;
             for (std::string n : notes) {
                 counter++;
                 std::cout << counter << ": " << n << std::endl << std::endl;
             }
         } else if (choice == 3) {
-            // TODO: edytuj notatki
+            std::cout << std::endl << "All your notes: " << std::endl;
+            for (std::string n : notes) {
+                counter++;
+                std::cout << counter << ": " << n << std::endl << std::endl;
+            }
+
+            int index;
+            std::cout << std::endl << "Which note do you want to edit?: " << std::endl;
+            std::cin >> index;
+            std::cin.ignore();
+
+            if (index > 0 && index <= notes.size()) {
+                std::string editedNote;
+
+                std::cout << "Edit your note" << std::endl;
+                std::getline(std::cin, editedNote);
+
+                notes[index - 1] = editedNote;
+
+                std::cout << "Edited note: " << editedNote << std::endl;
+            }
         } else if (choice == 4) {
-            // TODO: usuń notatkę
+            std::cout << std::endl << "All your notes: " << std::endl;
+            for (std::string n : notes) {
+                counter++;
+                std::cout << counter << ": " << n << std::endl << std::endl;
+            }
+
+            int index;
+            std::cout << std::endl << "Which note do you want to delete?: " << std::endl;
+            std::cin >> index;
+            std::cin.ignore();
+
+            if (index > 0 && index <= notes.size()) {
+                notes[index - 1].clear();
+            }
+
         } else if (choice == 5) {
             break;
         } else {
-            std::cout << "Pick a right number" << std::endl;
-            break;
+            std::cout << "Pick only available number" << std::endl;
+            isFalse = true;
         }
     }
     return 0;

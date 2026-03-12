06afcbb90b361ff29694dbb85f1b9b45390190a6
dawkam21 <dawid.jan.kaminski@gmail.com>
2026-03-12 09:19:16 +0100

    if else / need to change to switch

--- tic_tac_toe_cpp/main.cpp
+++ tic_tac_toe_cpp/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <algorithm>
+#include <list>
 
 void DisplayBoard(char Board[3][3]) {
+    std::cout << std::endl;
     for (int i = 0; i < 3; i++) { // i = 0, 1, 2 
         for (int j = 0; j < 3; j++) { // j = 0, 1, 2
             std::cout << Board[i][j] << " "; // '1' = Board[0, 0], '9' = Board[2, 2] 
         }
         std::cout << std::endl;
     }
+    std::cout << std::endl;
 }
 
 int main() {
@@ -22,20 +25,42 @@ int main() {
     std::string choice;
     std::cout << "Do you want to play Xs or Os?" << std::endl;
     std::cin >> choice;
+    std::cin.ignore();
+    
+    while (!isRunning) {
+        std::transform(choice.begin(), choice.end(), choice.begin(), ::tolower);
+        if (choice == "x") {
+            std::cout << "Pick a number" << std::endl;
+            DisplayBoard(Board);
+            int playersChoice;
+            char square[0][0];
+            std::list<int> squares;
+            std::cin >> playersChoice;
 
-    std::transform(choice.begin(), choice.end(), choice.begin(), ::tolower);
-    if (choice == "x") {
-        std::cout << "Pick a number to start the game!" << std::endl;
-        DisplayBoard(Board);
-        int square;
-        std::cin >> square;
-        if (square > 0 && square < 10) {
-            if (square == 5) {
+            if (playersChoice == 1) {
+                Board[0][0] = 'X';
+            } else if (playersChoice == 2) {
+                Board[0][1] = 'X';
+            } else if (playersChoice == 3) {
+                Board[0][2] = 'X';
+            } else if (playersChoice == 4) {
+                Board[1][0] = 'X';
+            } else if (playersChoice == 5) {
                 Board[1][1] = 'X';
-                DisplayBoard(Board);
+            } else if (playersChoice == 6) {
+                Board[1][2] = 'X';
+            } else if (playersChoice == 7) {
+                Board[2][0] = 'X';
+            } else if (playersChoice == 8) {
+                Board[2][1] = 'X';
+            } else if (playersChoice == 9) {
+                Board[2][2] = 'X';
+            } else {
+                std::cout << "Pick a right number!" << std::endl;
+                continue;
             }
+            DisplayBoard(Board);
         }
-    } 
-
+    }
     return 0;
 }

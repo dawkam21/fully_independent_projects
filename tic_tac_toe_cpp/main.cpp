514e26079e9be769cfcc2e90c7786d29235e7ed1
dawkam21 <dawid.jan.kaminski@gmail.com>
2026-03-15 10:01:07 +0100

    displaying board and naming players

--- tic_tac_toe_cpp/main.cpp
+++ tic_tac_toe_cpp/main.cpp
@@ -1,66 +1,80 @@
 #include <iostream>
 #include <algorithm>
-#include <list>
+#include <cctype>
+#include <string>
 
-void DisplayBoard(char Board[3][3]) {
-    std::cout << std::endl;
+class Board {
+public:
+    void DisplayBoard(char board[3][3]);
+    void ActualBoard(char board[3][3]);
+    int filledCells;
+};
+
+class Player {
+public:
+    char symbol;
+    std::string name;
+};
+
+void Board::DisplayBoard(char board[3][3]) {
+    std::cout << "-------------" << std::endl;
     for (int i = 0; i < 3; i++) { // i = 0, 1, 2 
+        std::cout << "| ";
         for (int j = 0; j < 3; j++) { // j = 0, 1, 2
-            std::cout << Board[i][j] << " "; // '1' = Board[0, 0], '9' = Board[2, 2] 
+            std::cout << board[i][j] << " | "; // '1' = Board[0, 0], '9' = Board[2, 2] 
         }
-        std::cout << std::endl;
+        std::cout << std::endl << "-------------" << std::endl;
+    }
+}
+
+void Board::ActualBoard(char board[3][3]) {
+    std::cout << std::endl;
+    std::cout << "-------------" << std::endl;
+    for (int i = 0; i < 3; i++) { // i = 0, 1, 2 
+        std::cout << "|  ";
+        for (int j = 0; j < 3; j++) { // j = 0, 1, 2
+            std::cout << " |  "; // '1' = Board[0, 0], '9' = Board[2, 2] 
+        }
+        std::cout << std::endl << "-------------" << std::endl;
     }
     std::cout << std::endl;
 }
 
 int main() {
 
-    char Board[3][3] = {
+    Board myBoard;
+    Player player;
+
+    char board[3][3] = {
         {'1', '2', '3'}, 
         {'4', '5', '6'}, 
         {'7', '8', '9'} 
     };
 
-    bool isRunning = false;
-    std::string choice;
-    std::cout << "Do you want to play Xs or Os?" << std::endl;
-    std::cin >> choice;
-    std::cin.ignore();
+    bool isPlaying = false;
     
-    while (!isRunning) {
-        std::transform(choice.begin(), choice.end(), choice.begin(), ::tolower);
-        if (choice == "x") {
-            std::cout << "Pick a number" << std::endl;
-            DisplayBoard(Board);
-            int playersChoice;
-            char square[0][0];
-            std::list<int> squares;
-            std::cin >> playersChoice;
+    std::cout << "Welcome to Tic Tac Toe Game!" << std::endl << "That's your board " << std::endl << std::endl;
+    myBoard.DisplayBoard(board);
 
-            if (playersChoice == 1) {
-                Board[0][0] = 'X';
-            } else if (playersChoice == 2) {
-                Board[0][1] = 'X';
-            } else if (playersChoice == 3) {
-                Board[0][2] = 'X';
-            } else if (playersChoice == 4) {
-                Board[1][0] = 'X';
-            } else if (playersChoice == 5) {
-                Board[1][1] = 'X';
-            } else if (playersChoice == 6) {
-                Board[1][2] = 'X';
-            } else if (playersChoice == 7) {
-                Board[2][0] = 'X';
-            } else if (playersChoice == 8) {
-                Board[2][1] = 'X';
-            } else if (playersChoice == 9) {
-                Board[2][2] = 'X';
-            } else {
-                std::cout << "Pick a right number!" << std::endl;
-                continue;
-            }
-            DisplayBoard(Board);
+    std::cout << "Do you want to play Xs or Os?" << std::endl << std::endl;
+    
+    while (!isPlaying) {
+        std::string choice;
+        std::cin >> choice;
+        std::transform(choice.begin(), choice.end(), choice.begin(), [](unsigned char c) { return std::toupper(c); });
+        if (choice == "X") {
+            player.name = "Player X";
+            player.symbol = 'X';
+        } else if (choice == "O") {
+            player.name = "Player O";
+            player.symbol = 'O';
+        } else {
+            std::cout << "Pick valid symbol." << std::endl;
         }
+        std::cout << std::endl << "You are " << player.name << " from now, let's start the game. Choose square from 1 to 9." << std::endl;
+
+        myBoard.ActualBoard(board);
     }
+
     return 0;
 }
